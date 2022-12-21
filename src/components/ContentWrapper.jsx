4ead6e8aa510a32051361019bef70bc3129e1c3a
Ezequiel Garcia <ehgarcia@outlook.com.ar>
2022-12-20 21:57:37 -0300

    Practicando pasar una pagina en HTML y CSS a React

---
+++ src/components/ContentWrapper.jsx
@@ -0,0 +1,18 @@
+import React,{Fragment} from "react";
+import { TopBar } from "./TopBar";
+import { Footer } from "./Footer";
+import { ContentRowTop } from './ContentRowTop'
+
+export function ContentWrapper() {
+  return (
+    <Fragment>
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <TopBar />
+        <ContentRowTop />
+      </div>
+      <Footer />
+    </div>
+    </Fragment>
+  );
+}
